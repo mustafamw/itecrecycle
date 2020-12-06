3ee3aadc75cd111c89f1190d495a897bf69860a8
Mustafa Mohamed <mo.mustafa3@gmail.com>
2020-12-06 20:48:22 +0000

    updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,6 @@
-const configs = {
-  default: {
-    app: {
-      api: 'http://localhost:3001'
-    }
-  },
-  localhost: {
-    app: {
-      api: 'http://localhost:3001'
-    }
-  },
-  production: {
-    app: {
-      api: 'http://localhost:3001'
-    }
+export const config = {
+  app: {
+    api: process.env.REACT_APP_API,
+    environment: process.env.REACT_APP_ENVIRONMENT,
   }
 }
-
-export const config = configs[process.env.REACT_APP_ENVIRONMENT] ? configs[process.env.REACT_APP_ENVIRONMENT] : configs.default;
